689825f5f97748e583fc0728b7754e0cc0f9855b
Lukasz Marcul <dev.marcul@gmail.com>
2018-12-01 19:41:42 +0100

    c: Implement solver interface.

---
+++ c/src/solver.c
@@ -0,0 +1,111 @@
+#include "solver.h"
+
+#include <string.h>
+#include <stdlib.h>
+#include <float.h>
+#include <omp.h>
+#include <stdio.h>
+
+#include "memory.h"
+#include "exception.h"
+
+static const char* solver_names[] = {
+};
+
+solver_t* solver_create(solver_type_t type, const graph_t* graph)
+{
+    if (type >= SOLVER_TOP) {
+        exception_throw(EX_ARGUMENT, "Invalid solver type!");
+    }
+
+    if (!graph) {
+        exception_throw(EX_ARGUMENT, "Graph to solve invalid!");
+    }
+
+    if (graph->size < 2) {
+        exception_throw(EX_ARGUMENT, "Graph size should be equal to 2 minimally");
+    }
+
+    const size_t path_len = graph->size + 1;
+    solver_t* solver = MALLOC(solver_t, sizeof(solver_t));
+    solver->graph = graph;
+    solver->solved = false;
+    solver->best_path = path_create(path_len);
+    memset(solver->best_path->sequence, 0, path_len * sizeof(*(solver->best_path->sequence)));
+    solver->best_path->len = path_len;
+    solver->best_path->cost = DBL_MAX;
+
+    switch (type)
+    {
+        default:
+            break;
+    }
+
+    solver->type = type;
+
+    return solver;
+}
+
+void solver_delete(solver_t** solver)
+{
+    if (!(*solver)) {
+        return;
+    }
+
+    path_delete(&(*solver)->best_path);
+    FREE(*solver);
+}
+
+void solver_run(solver_t* solver)
+{
+    if (!solver) {
+        return;
+    }
+
+    solver->start_time = omp_get_wtime();
+
+    solver->solved = solver->solve(solver->graph, solver->best_path, solver->graph->size);
+
+    solver->stop_time = omp_get_wtime();
+}
+
+void solver_run_in_parallel(solver_t* solver, size_t threads_nr)
+{
+    if (!solver) {
+        return;
+    }
+
+    solver->start_time = omp_get_wtime();
+
+    solver->solved = solver->solve(solver->graph, solver->best_path, threads_nr);
+
+    solver->stop_time = omp_get_wtime();
+}
+
+void solver_show(const solver_t* solver)
+{
+    if (!solver) {
+        return;
+    }
+
+    const char* name = solver_get_name(solver);
+
+    if (!solver->solved) {
+        printf("[%s] Not solved!\n", name);
+        return;
+    }
+
+    printf("%s\n", name);
+    printf("Time: %.6lf s\n", solver->stop_time - solver->start_time);
+    printf("Best path: ");
+    path_show(solver->best_path);
+}
+
+const char* solver_get_name(const solver_t* solver)
+{
+    if (!solver) {
+        return NULL;
+    }
+
+    return solver_names[solver->type];
+}
---
+++ c/src/solver.h
@@ -0,0 +1,55 @@
+#ifndef TSP_SOLVE_SOLVER_H
+#define TSP_SOLVE_SOLVER_H
+
+#include <stdbool.h>
+
+#include "graph.h"
+#include "path.h"
+
+typedef enum {
+    SOLVER_TOP
+} solver_type_t;
+
+typedef bool (*solver_if)(const graph_t* graph, path_t* best_path, size_t param);
+
+typedef struct {
+    solver_type_t type;
+    solver_if solve;
+    bool solved;
+    path_t* best_path;
+    const graph_t* graph;
+    double start_time;
+    double stop_time;
+} solver_t;
+
+/*
+ * @brief Create solver for Travelling Salesman Problem.
+ */
+solver_t* solver_create(solver_type_t type, const graph_t* graph);
+
+/*
+ * @brief Delete remaining resources.
+ */
+void solver_delete(solver_t** solver);
+
+/*
+ * @brief Solve problem sequentially.
+ */
+void solver_run(solver_t* solver);
+
+/*
+ * @brief Solve problem in parallel.
+ */
+void solver_run_in_parallel(solver_t* solver, size_t threads_nr);
+
+/*
+ * @brief Print solver results.
+ */
+void solver_show(const solver_t* solver);
+
+/*
+ * @brief Get solver name.
+ */
+const char* solver_get_name(const solver_t* solver);
+
+#endif //TSP_SOLVE_SOLVER_H
