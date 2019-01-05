e08a684c91845f3b9780eca5abfdba22451d8754
Lukasz Marcul <dev.marcul@gmail.com>
2019-01-05 14:35:49 +0100

    c: Implement state interface.

---
+++ c/src/state.h
@@ -0,0 +1,20 @@
+#ifndef TSP_SOLVE_STATE_H
+#define TSP_SOLVE_STATE_H
+
+#include <stdbool.h>
+
+#include "solver.h"
+
+typedef enum {
+    STATE_TOP
+} state_t;
+
+typedef struct {
+    const char* graph_file_name;
+    size_t size;
+    bool generated;
+    size_t threads_nr;
+    solver_type_t solver_type;
+} state_ctx_t;
+
+#endif //TSP_SOLVE_STATE_H
